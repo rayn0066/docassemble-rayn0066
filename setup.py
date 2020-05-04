6fd7bb090a26c691fa6e3db9ec87e015231343dd
Rebecca Rayner <61669083+rayn0066@users.noreply.github.com>
2020-05-04 12:34:31 +0930

    test

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.rayn0066',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.rayn0066\n\nA docassemble extension.\n\n## Author\n\nRebecca Rayner, rayn0066@flinders.edu.au\n\n',
+      long_description='# docassemble.rayn0066\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nRebecca Rayner, rayn0066@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Rebecca Rayner',
       author_email='rayn0066@flinders.edu.au',
